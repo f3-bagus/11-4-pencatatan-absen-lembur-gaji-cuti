f7b30af999345356344e1661223740d092eb11f1
Vannnisa <vannyssa24@gmail.com>
2024-06-16 00:32:13 +0700

    profile

--- frontend/src/layouts/admin/AdminLayout.jsx
+++ frontend/src/layouts/admin/AdminLayout.jsx
@@ -1,70 +1,60 @@
 import React, { useState } from "react";
-import { Flex, HStack, Button, Text, useColorModeValue } from "@chakra-ui/react";
-import { MdMenu } from "react-icons/md";
+import { Flex, HStack, useColorModeValue } from "@chakra-ui/react";
 import { Sidebar } from "../../components/admin/Sidebar";
 import Topbar from "../../components/admin/Topbar";
 
-const AdminLayout = ({children}) => {
-  const [collapse, setCollapse] = useState(false);
-
-  return (
-    <HStack w="full" h="100vh" bg={useColorModeValue("gray.100", "green.900")} padding={6}>
-      {/* Sidebar */}
-      <Flex
-        as="aside"
-        w={collapse ? "0" : "250px"}
-        h="full"
-        bg={useColorModeValue("white", "green.800")}
-        alignItems="center"
-        justifyContent="space-between"
-        padding={6}
-        flexDirection="column"
-        borderRadius="2xl"
-        transition="width 0.4s ease"
-        display={{ base: "none", md: "block" }}
-      >
-        <Sidebar collapse={collapse} />
-      </Flex>
-
-      <Flex w="full" h="full" flexDirection="column" gap={2}>
-
-        {/* Header */}
+const adminLayout = ({children}) => {
+    const [collapse, setCollapse] = useState(false);
+  
+    return (
+      <HStack w="full" h="100vh" bg={useColorModeValue("gray.100", "green.900")} padding={6}>
+        {/* Sidebar */}
         <Flex
-          as="header"
-          w="full"
+          as="aside"
+          w={collapse ? "0" : "250px"}
+          h="full"
           bg={useColorModeValue("white", "green.800")}
           alignItems="center"
-          padding={4}
-          borderRadius="2xl"
-        >
-          {/* <Button
-            display={{ base: "none", md: "block" }}
-            aria-label="Menu Collapse"
-            onClick={() => setCollapse(!collapse)}
-          ><MdMenu /></Button> */}
-          <Topbar/>
-        </Flex>
-
-        {/* Main */}
-        <Flex
-          as="main"
-          w="full"
-          h="full"
-          bg={useColorModeValue("gray.100", "green.900")}
+          justifyContent="space-between"
+          padding={6}
           flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
           borderRadius="2xl"
-          overflow="scroll"
+          transition="width 0.4s ease"
+          display={{ base: "none", md: "block" }}
         >
-          <Text fontSize={100} color="gray.500">
-            {children}
-          </Text>
+          <Sidebar collapse={collapse} />
         </Flex>
-
-      </Flex>
-    </HStack>
-  );
-};
-
-export default AdminLayout;
+  
+        <Flex w="full" h="full" flexDirection="column" gap={2}>
+  
+          {/* Header */}
+          <Flex
+            as="header"
+            w="full"
+            bg={useColorModeValue("white", "green.800")}
+            alignItems="center"
+            padding={4}
+            borderRadius="2xl"
+          >
+            <Topbar/>
+          </Flex>
+  
+          {/* Main */}
+          <Flex
+            as="main"
+            w="full"
+            h="full"
+            flexDirection="row"
+            bg={useColorModeValue("gray.100", "green.900")}
+            borderRadius="2xl"
+            overflow="scroll"
+          >
+            {children}
+          </Flex>
+  
+        </Flex>
+      </HStack>
+    );
+  };
+  
+  export default adminLayout;
--- frontend/src/layouts/admin/navigation/Profile.jsx
+++ frontend/src/layouts/admin/navigation/Profile.jsx
@@ -1,12 +1,195 @@
-import React from 'react';
-import AdminLayout from '../AdminLayout';
+import React, { useState, useEffect } from "react";
+import AdminLayout from "../AdminLayout";
+import {
+  Box,
+  Avatar,
+  Text,
+  VStack,
+  Flex,
+  useColorModeValue,
+  Stack,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 const Profile = () => {
+  const [name, setName] = useState("");
+  const [nip, setNip] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const getProfile = () => {
+    axios
+      .get("http://localhost:5000/api/user/profile")
+      .then((res) => {
+        //console.log(res.data.data);
+        setName(res.data.data.name);
+        setNip(res.data.data.nip);
+        setEmail(res.data.data.email);
+        setPhone(res.data.data.phone);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <AdminLayout>
-        Admin Profile
-    </AdminLayout>
-  )
-}
+      <Flex w="full" p="5" direction={{ base: "column", md: "row" }} gap={5}>
+        <Flex w="full" direction="column" gap={5}>
+          <Box
+            w="full"
+            borderRadius="2xl"
+            boxShadow="lg"
+            bg={useColorModeValue("white", "green.800")}
+          >
+            <Box
+              borderTopRadius="2xl"
+              h="150px"
+              bgGradient="linear(to-b, green.200, green.500)"
+            />
+            <VStack mt="-12" spacing="3" mb={5}>
+              <Avatar
+                size="2xl"
+                src=""
+                borderColor="white"
+                bgColor="gray.500"
+              />
+              <Text fontSize="lg" fontWeight="bold">
+                {name}
+              </Text>
+              <Text fontSize="md" color="gray.500">
+                Account type: <b>Admin</b>
+              </Text>
+            </VStack>
+          </Box>
+          <Box
+            w="full"
+            borderRadius="2xl"
+            boxShadow="lg"
+            p={5}
+            bg={useColorModeValue("white", "green.800")}
+          >
+            <VStack align="flex-start" mb={8}>
+              <Heading as="h2" size="lg">
+                Change password
+              </Heading>
+              <Text fontSize="md" color="gray.500">
+                Here you can set your new password
+              </Text>
+            </VStack>
+            <Stack py={2} direction="column">
+              <FormControl>
+                <FormLabel>Old Password</FormLabel>
+                <Input
+                  type="text"
+                  focusBorderColor="green.500"
+                  placeholder="Old Password"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>New Password</FormLabel>
+                <Input
+                  type="text"
+                  focusBorderColor="green.500"
+                  placeholder="New Password"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>New Password Confirmation</FormLabel>
+                <Input
+                  type="text"
+                  focusBorderColor="green.500"
+                  placeholder="Confirm New Password"
+                />
+              </FormControl>
+            </Stack>
+            <Button colorScheme="green" float="right" mt={3}>
+              Change Password
+            </Button>
+          </Box>
+        </Flex>
 
-export default Profile
+        {/* Edit Account */}
+        <Box
+          w="full"
+          maxH={{ base: "650px", md: "450px" }}
+          borderRadius="2xl"
+          boxShadow="lg"
+          bg={useColorModeValue("white", "green.800")}
+          p={5}
+        >
+          <VStack align="flex-start" mb={8}>
+            <Heading as="h2" size="lg">
+              Account Settings
+            </Heading>
+            <Text fontSize="md" color="gray.500">
+              Here you can change user account information
+            </Text>
+          </VStack>
+          <Stack py={2} direction={{ base: "column", md: "row" }}>
+            <FormControl>
+              <FormLabel>NIP</FormLabel>
+              <Input
+                type="text"
+                focusBorderColor="green.500"
+                placeholder={nip}
+                disabled
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Fullname</FormLabel>
+              <Input
+                type="text"
+                focusBorderColor="green.500"
+                placeholder={name}
+              />
+            </FormControl>
+          </Stack>
+          <Stack py={2} direction={{ base: "column", md: "row" }}>
+            <FormControl>
+              <FormLabel>Email Address</FormLabel>
+              <Input
+                type="email"
+                focusBorderColor="green.500"
+                placeholder={email}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Phone Number</FormLabel>
+              <Input
+                type="number"
+                focusBorderColor="green.500"
+                placeholder={phone}
+              />
+            </FormControl>
+          </Stack>
+          <Stack py={2} direction={{ base: "column", md: "row" }}>
+            <FormControl>
+              <FormLabel>Profile Picture</FormLabel>
+              <Input
+                type="file"
+                focusBorderColor="green.500"
+                accept="image/jpeg, image/png, image/jpg"
+                alignContent="center"
+              />
+            </FormControl>
+          </Stack>
+          <Button colorScheme="green" float="right" mt={3}>
+            Edit
+          </Button>
+        </Box>
+      </Flex>
+    </AdminLayout>
+  );
+};
+
+export default Profile;
